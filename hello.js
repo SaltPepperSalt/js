964e7718e4fb013e4ab9a47cc7642d1b4c80eed5
SaltPepperSalt <dksro324@gmail.com>
2020-05-13 21:29:52 +0900

    TIL: Class practice

--- hello.js
+++ hello.js
@@ -1,18 +1,21 @@
-function bubbleSort(array) {
-  const { length } = array;
-  let change = 0;
-  for (let j = 0; j < length - 1; j++) {
-    for (let i = 0; i < length; i++) {
-      if (array[i] > array[i + 1]) {
-        change = array[i];
-        array[i] = array[i + 1];
-        array[i + 1] = change;
-      }
-    }
+class Person {
+  constructor(fN, lN) {
+    this.fN = fN;
+    this.lN = lN;
   }
-  return array;
+
+  get fullName() {
+    return `${this.fN} ${this.lN}`;
+  }
+
+  set fullName(name) {
+    [this.fN, this.lN] = name.split(' ');
+  }
 }
 
-console.log(bubbleSort([2, 4, 5, 1, 3]));// [1, 2, 3, 4, 5]
-console.log(bubbleSort([5, 2, 1, 3, 4, 6]));// [1, 2, 3, 4, 5, 6]
-console.log(bubbleSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
+
+const kim = new Person('Jin', 'Yu');
+console.log(kim);
+console.log(kim.fullName);
+kim.fullName = 'Min mi';
+console.log(kim.fullName);
