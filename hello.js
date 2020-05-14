31f01bace7bcd987a73b0c108f1755b5df0ad36f
SaltPepperSalt <dksro324@gmail.com>
2020-05-14 15:16:24 +0900

    InsertionSort

--- hello.js
+++ hello.js
@@ -1,23 +1,18 @@
-function selectionSort(array) {
-  let index = 0;
-  let change = 0;
+function insertionSort(array) {
   const { length } = array;
   for (let j = 0; j < length - 1; j++) {
-    let min = array[j];
-    for (let i = j; i < length; i++) {
-      if (min > array[i]) {
-        min = array[i];
-        index = i;
+    for (let i = j + 1; i >= 0; i--) {
+      if (array[i - 1] > array[i]) {
+        let change = 0;
+        change = array[i - 1];
+        array[i - 1] = array[i];
+        array[i] = change;
       }
     }
-    change = array[j];
-    array[j] = array[index];
-    array[index] = change;
   }
-
   return array;
 }
 
-console.log(selectionSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
-console.log(selectionSort([2, 4, 5, 1, 3])); // [1, 2, 3, 4, 5]
-console.log(selectionSort([5, 2, 1, 3, 4, 6])); // [1, 2, 3, 4, 5, 6]
+console.log(insertionSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
+console.log(insertionSort([2, 4, 5, 1, 3])); // [1, 2, 3, 4, 5]
+console.log(insertionSort([5, 2, 1, 3, 4, 6])); // [1, 2, 3, 4, 5, 6]
