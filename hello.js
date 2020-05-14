9746e88c394527a6467289983b59a44544d97e15
SaltPepperSalt <dksro324@gmail.com>
2020-05-14 15:03:13 +0900

    selectionSort

--- hello.js
+++ hello.js
@@ -1,21 +1,23 @@
-class Person {
-  constructor(fN, lN) {
-    this.fN = fN;
-    this.lN = lN;
+function selectionSort(array) {
+  let index = 0;
+  let change = 0;
+  const { length } = array;
+  for (let j = 0; j < length - 1; j++) {
+    let min = array[j];
+    for (let i = j; i < length; i++) {
+      if (min > array[i]) {
+        min = array[i];
+        index = i;
+      }
+    }
+    change = array[j];
+    array[j] = array[index];
+    array[index] = change;
   }
 
-  get fullName() {
-    return `${this.fN} ${this.lN}`;
-  }
-
-  set fullName(name) {
-    [this.fN, this.lN] = name.split(' ');
-  }
+  return array;
 }
 
-
-const kim = new Person('Jin', 'Yu');
-console.log(kim);
-console.log(kim.fullName);
-kim.fullName = 'Min mi';
-console.log(kim.fullName);
+console.log(selectionSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
+console.log(selectionSort([2, 4, 5, 1, 3])); // [1, 2, 3, 4, 5]
+console.log(selectionSort([5, 2, 1, 3, 4, 6])); // [1, 2, 3, 4, 5, 6]
